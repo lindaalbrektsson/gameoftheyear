84cf6b780e8511f21fd5101f6dec41d48c27cb99
Isabell <isabellviolaamanda@gmail.com>
2026-04-20 15:43:22 +0200

    Update start page naming and styles

--- src/modules/startPage.ts
+++ src/modules/startPage.ts
@@ -1,28 +1,30 @@
 export function renderStartPage() {
-    const startPage = document.createElement('div');
-    startPage.classList.add('start-page');
+  const startPage = document.createElement("div");
+  startPage.classList.add("start-page");
 
-    const howToPlay = document.createElement('p');
-    howToPlay.classList.add('how-to-play');
+  const howToPlay = document.createElement("section");
+  howToPlay.classList.add("how-to-play");
 
-    const howToPlayText = document.createElement('p');
-    howToPlayText.textContent = "HOW TO PLAY?";
-    howToPlay.appendChild(howToPlayText);
+  const howToPlayText = document.createElement("p");
+  howToPlayText.textContent = "HOW TO PLAY?";
+  howToPlay.appendChild(howToPlayText);
 
-    const playerForm = document.createElement('p');
-    playerForm.classList.add('player-form');
-    const nameInput = document.createElement('input');
-    nameInput.type = 'text';
-    nameInput.placeholder = 'Enter your name';
-    playerForm.appendChild(nameInput);
+  const playerForm = document.createElement("section");
+  playerForm.classList.add("player-form");
 
-    const startButton = document.createElement('button');
-    startButton.classList.add('start-button');
-    startButton.textContent = 'Start Game';
-    playerForm.appendChild(startButton);
+  const playerNameInput = document.createElement("input");
+  playerNameInput.type = "text";
+  playerNameInput.classList.add("player-name-input");
+  playerNameInput.placeholder = "Enter your name";
+  playerForm.appendChild(playerNameInput);
 
-    startPage.appendChild(howToPlay);
-    startPage.appendChild(playerForm);
+  const startGameBtn = document.createElement("button");
+  startGameBtn.classList.add("start-game-button");
+  startGameBtn.textContent = "Start Game";
+  playerForm.appendChild(startGameBtn);
 
-    return startPage;
+  startPage.appendChild(howToPlay);
+  startPage.appendChild(playerForm);
+
+  return startPage;
 }
