3c3b425148f036d76b7ec6ca70714b331a60c19f
Linda Albrektsson <linda.albrektsson@gmail.com>
2026-04-20 12:22:06 +0200

    Imported modules and styles o main.ts

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "./SCSS/styles.scss";
+import { renderStartPage } from "./modules/startPage";
+import { renderActivePlayerStartPage } from "./modules/activePlayerStartPage";
+import { renderInGame } from "./modules/inGame";
+import { renderGameOver } from "./modules/gameOver";
+
+renderStartPage();
