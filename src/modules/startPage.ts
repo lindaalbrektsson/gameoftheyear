29c98bfb92fc963277c1c9557f6461bc7b1bb94e
Isabell <isabellviolaamanda@gmail.com>
2026-04-20 13:30:05 +0200

    Create input field for player name

--- src/modules/startPage.ts
+++ src/modules/startPage.ts
@@ -2,14 +2,22 @@ export function renderStartPage() {
     const startPage = document.createElement('div');
     startPage.classList.add('start-page');
 
-    const howToPlay = document.createElement('section');
+    const howToPlay = document.createElement('p');
     howToPlay.classList.add('how-to-play');
 
     const howToPlayText = document.createElement('p');
-    howToPlayText.textContent = "HOW TOP PLAY?";
+    howToPlayText.textContent = "HOW TO PLAY?";
     howToPlay.appendChild(howToPlayText);
 
+    const playerForm = document.createElement('p');
+    playerForm.classList.add('player-form');
+    const nameInput = document.createElement('input');
+    nameInput.type = 'text';
+    nameInput.placeholder = 'Enter your name';
+    playerForm.appendChild(nameInput);
+
     startPage.appendChild(howToPlay);
+    startPage.appendChild(playerForm);
 
     return startPage;
 }
