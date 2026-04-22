31ddae213e15687daba4454a932cf4a837c99b49
hannakinnander <kinnander95@live.se>
2026-04-22 16:27:25 +0200

    spend a lot of time figuring out the flow of the functions before the game actually starts. Fixed the countdown, that shows when the inGame page is created and the initial values (level, score, lives and time left) has been rendered aswell. I made a function that collects these steps before the game really starts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,1 @@
 import "./styles.scss";
-import { renderInGame } from "./modules/inGame";
-import { startCountdown } from "./modules/countdown";
-import { renderStartPage } from "./modules/startPage";
-import { renderActivePlayerStartPage } from "./modules/activePlayerStartPage";
-
-// const activePlayer = localStorage.getItem("activePlayer");
-
-// if (activePlayer) {
-//   renderActivePlayerStartPage();
-// } else {
-//   renderStartPage();
-// }
-
-renderInGame();
-startCountdown();
