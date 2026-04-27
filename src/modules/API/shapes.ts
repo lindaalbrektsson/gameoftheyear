aef987109d8134a59f5a4bd74ce415209abfbde9
Linda Albrektsson <linda.albrektsson@gmail.com>
2026-04-27 23:42:57 +0200

    Added Linda test version for in-game rule flow

--- src/modules/API/shapes.ts
+++ src/modules/API/shapes.ts
@@ -1,12 +1,20 @@
-export async function getShapes(): Promise<any[]> {
-   
+//Måste skriva interface för shapes och typa upp funktionen
+
+//LINDA: Jag gjorde ett interface, kolla om du vill ändra något, jag behövde den för min testfil så att den inte skulle tycka att shapes är any[] hela tiden.
+export interface Shape {
+    id: string;
+    type: "triangle" | "square" | "circle" | "star";
+    color: "blue" | "red" | "yellow" | "green" | "orange" | "purple" | "blank";
+    difficultyLevel: number;
+}
+
+export async function getShapes(): Promise<Shape[]> {
     const response = await fetch("http://localhost:3000/shapes");
 
     if (!response.ok) {
-        throw new Error ("Couldn't get shapes");
+        throw new Error("Couldn't get shapes");
     }
+
     const data = await response.json();
     return data;
-};
-
-//Måste skriva interface för shapes och typa upp funktionen
+}
