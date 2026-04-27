b1657a108652bb05b87de963ecfe2095a34c2aa2
hannakinnander <kinnander95@live.se>
2026-04-27 14:40:59 +0200

    added interface and ShapeType

--- src/modules/API/shapes.ts
+++ src/modules/API/shapes.ts
@@ -1,3 +1,18 @@
+import type { ColorType } from "./instructions";
+
+interface Shape {
+    id: "string",
+    type: ShapeType,
+    color: ColorType,
+    difficultyLevel: number
+}
+
+type ShapeType = 
+| "triangle"
+| "square"
+| "circle"
+| "star";
+
 export async function getShapes(): Promise<any[]> {
    
     const response = await fetch("http://localhost:3000/shapes");
